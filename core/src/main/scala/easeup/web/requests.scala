8a81b42aa31727e108b2e05d106509f1501cfe38
Tamas Neltz <tamas.neltz@gmail.com>
2018-03-18 23:24:53 -0400

    clean up conversion

--- core/src/main/scala/easeup/web/requests.scala
+++ core/src/main/scala/easeup/web/requests.scala
@@ -13,11 +13,16 @@ case object JsonNull extends JsonValue
 final case class JsonBoolean(x: Boolean) extends JsonValue
 final case class JsonString(x: String) extends JsonValue
 final case class JsonNumber(x: Float) extends JsonValue
-final case class JsonArray(xs: Seq[JsonValue]) extends JsonValue
+final case class JsonArray(xs: Traversable[JsonValue]) extends JsonValue
 final case class JsonObject(xs: List[(String, JsonValue)]) extends JsonValue
 
-trait RequestFieldEncoder[A] {
+trait RequestFieldEncoder[-A] {
   def asJsonValue(x: A): JsonValue
+  def asString(x: A): String
+}
+
+trait RequestObjectEncoder[A] extends RequestFieldEncoder[A] {
+  def asJsonValue(x: A): JsonObject
   def asString(x: A): String
 }
 
@@ -26,6 +31,11 @@ object RequestFieldEncoder {
 
   def instance[A](f: A => JsonValue, g: A => String): RequestFieldEncoder[A] = new RequestFieldEncoder[A] {
     override def asJsonValue(x: A): JsonValue = f(x)
+    override def asString(x: A): String = g(x)
+  }
+
+  def objectInstance[A](f: A => JsonObject, g: A => String): RequestObjectEncoder[A] = new RequestObjectEncoder[A] {
+    override def asJsonValue(x: A): JsonObject = f(x)
     override def asString(x: A): String = g(x)
   }
 
@@ -38,29 +48,19 @@ object RequestFieldEncoder {
   implicit val double: RequestFieldEncoder[Double] = instance[Double](x => JsonNumber(x.toFloat), _.toString)
 
   implicit val boolean: RequestFieldEncoder[Boolean] = instance[Boolean](JsonBoolean(_), _.toString)
-}
 
-trait RequestObjectEncoder[A] extends RequestFieldEncoder[A] {
-  override def asJsonValue(x: A): JsonObject
-  def asString(x: A): String
-}
+  implicit def list[A](implicit enc: RequestFieldEncoder[A]): RequestFieldEncoder[Traversable[A]] =
+    instance(xs => JsonArray(xs.map(enc.asJsonValue)), xs => xs.map(enc.asString).mkString(","))
 
-object RequestObjectEncoder {
-  def apply[A](implicit enc: RequestObjectEncoder[A]): RequestObjectEncoder[A] = enc
-
-  def instance[A](f: A => JsonObject, g: A => String): RequestObjectEncoder[A] = new RequestObjectEncoder[A] {
-    override def asJsonValue(x: A): JsonObject = f(x)
-    override def asString(x: A): String = g(x)
-  }
-
-  implicit val hnil: RequestObjectEncoder[HNil] = instance(_ => JsonObject(List.empty[(String, JsonValue)]), _ => "")
+  implicit val hnil: RequestObjectEncoder[HNil] =
+    objectInstance(_ => JsonObject(List.empty[(String, JsonValue)]), _ => "")
 
   implicit def hlist[K <: Symbol, H, T <: HList](implicit
     witness: Witness.Aux[K],
     henc: Lazy[RequestFieldEncoder[H]],
     tenc: RequestObjectEncoder[T]): RequestObjectEncoder[FieldType[K, H] :: T] = {
     val fieldName: String = witness.value.name
-    instance(hlist => {
+    objectInstance(hlist => {
       val head = henc.value.asJsonValue(hlist.head)
       val tail = tenc.asJsonValue(hlist.tail)
       JsonObject((fieldName -> head) :: tail.xs)
@@ -73,9 +73,8 @@ object RequestObjectEncoder {
 
   implicit def generic[A, H](implicit
     generic: LabelledGeneric.Aux[A, H],
-    henc: Lazy[RequestObjectEncoder[H]]): RequestObjectEncoder[A] = instance(x => {
+    henc: Lazy[RequestObjectEncoder[H]]): RequestFieldEncoder[A] = RequestFieldEncoder.instance(x => {
     henc.value.asJsonValue(generic.to(x))
-
   }, x => {
     henc.value.asString(generic.to(x))
   })

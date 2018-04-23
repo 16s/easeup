c6f703f02f7a2d89270ff0666218fc4db06ea2fb
Tamas Neltz <tamas.neltz@gmail.com>
2018-04-22 21:36:36 -0400

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,8 +3,9 @@ import sbt._
 object Dependencies {
   object Versions {
     val shapeless = "2.3.3"
-    val cats = "1.0.1"
-    val circe = "0.9.1"
+    val cats = "1.1.0"
+    val circe = "0.9.3"
+    val http4s = "0.18.9"
   }
 
   val commonDependencies = Seq(
@@ -17,7 +18,7 @@ object Dependencies {
     "org.typelevel" %% "cats-kernel" % Versions.cats,
     "org.typelevel" %% "cats-macros" % Versions.cats,
     "org.typelevel" %% "cats-effect" % "0.10",
-    "net.16shells" %% "result" % "0.0.1"
+    "net.16shells" %% "result" % "0.0.2"
   )
 
   val circe = commonDependencies ++ Seq(
@@ -26,5 +27,9 @@ object Dependencies {
     "io.circe" %% "circe-parser"
   ).map(_ % Versions.circe)
 
-  val http4s = commonDependencies
+  val http4s = commonDependencies ++ Seq(
+    "org.http4s" %% "http4s-dsl" % Versions.http4s,
+    "org.http4s" %% "http4s-blaze-server" % Versions.http4s,
+    "org.http4s" %% "http4s-blaze-client" % Versions.http4s
+  )
 }
